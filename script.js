34406a9de9eb6d311fc1c61c53274b7700375b4c
Dmitry <157232673+YomsViking@users.noreply.github.com>
2025-05-22 15:52:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const expandableServices = document.querySelectorAll('.expandable');
-
-  expandableServices.forEach(service => {
-    service.addEventListener('click', () => {
-      service.classList.toggle('open');
-    });
-  });
-});
+function selectCategory(category) {
+  console.log("Выбрана категория:", category);
+  // Здесь будет переход к следующей странице
+  // Например: window.location.href = category.html?type=${category};
+}
