0acab448f036cddcc2780f87b4566d6d3fd67890
Dmitry <157232673+YomsViking@users.noreply.github.com>
2025-12-24 09:33:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function handleClick(category) {
   switch (category) {
-    case 'astrology': window.location.href = 'astrology.html'; break;
-    case 'numerology': window.location.href = 'numerology.html'; break;
-    case 'tarot': window.location.href = 'tarot.html'; break;
-    case 'coffee': window.location.href = 'coffee.html'; break;
-    case 'human': window.location.href = 'human.html'; break;
-    default: alert('Категория не найдена');
+    case 'astrology':
+      window.location.href = 'astrology.html';
+      break;
+
+    case 'tarot':
+      window.location.href = 'tarot.html';
+      break;
+
+    case 'numerology':
+      window.location.href = 'numerology.html';
+      break;
+
+    default:
+      console.warn('Неизвестная категория:', category);
   }
 }
