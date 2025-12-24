64c28ab8f292f3441140364f99d2724777d86ba1
Dmitry <157232673+YomsViking@users.noreply.github.com>
2025-12-24 21:10:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleClick(category) {
+  switch (category) {
+    case 'astrology': window.location.href = 'astrology.html'; break;
+    case 'numerology': window.location.href = 'numerology.html'; break;
+    case 'tarot': window.location.href = 'tarot.html'; break;
+  }
+}
